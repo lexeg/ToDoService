281171ab0e3e1b958df18b96780b9abb3aaf9d67
petrenko-a <p>
2024-03-07 17:54:56 +0500

    Вынес в отдельный класс Startup настройку сервиса и добавление зависимостей.

--- ToDoService/ToDoService/Program.cs
+++ ToDoService/ToDoService/Program.cs
@@ -4,13 +4,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllers();
-        var app = builder.Build();
-
-        app.MapGet("/", () => "Hello World!");
-        app.MapControllers();
-
-        app.Run();
+        CreateHostBuilder(args).Build().Run();
     }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 }
