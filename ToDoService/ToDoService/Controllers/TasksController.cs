de928360517d8d0cb2685e0112636268e2b09371
petrenko-a <p>
2024-03-07 17:29:40 +0500

    Создал контроллер для работы со списком задач.
    Добавил два метода с фейковыми данными.

---
+++ ToDoService/ToDoService/Controllers/TasksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TasksController : ControllerBase
+{
+    [HttpGet]
+    public Task<List<string>> GetTasks()
+    {
+        return Task.FromResult(new List<string>
+        {
+            "задача 1",
+            "задача 2",
+            "задача 3"
+        });
+    }
+
+    [HttpGet("{id}")]
+    public Task<string> GetById(int id)
+    {
+        return Task.FromResult($"задача {id}");
+    }
+}
