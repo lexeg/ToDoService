60d3245565876e0b97163b9e65cc26829787a875
petrenko-a <p>
2024-03-07 17:54:56 +0500

    Вынес в отдельный класс Startup настройку сервиса и добавление зависимостей.

---
+++ ToDoService/ToDoService/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace ToDoService;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", () => "Hello World!");
+            endpoints.MapControllers();
+        });
+    }
+}
